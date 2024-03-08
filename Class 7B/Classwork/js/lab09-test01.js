9f6ed7ce93a2304e4ae3dd0a36ca9c3db008265a
etamiller <ermiller204@gmail.com>
2024-03-07 20:49:47 -0500

    classwork7b

---
+++ Class 7B/Classwork/js/lab09-test01.js
@@ -0,0 +1,18 @@
+/* add code after this comment */
+document.getElementById('thumb-list').style.border = "solid 1px #CCCCCC";
+
+// document.querySelector('#msg').value = 'Hello';
+document.getElementById('thumb-list').classList;
+document.querySelector('#msg').value = document.querySelector('.box fieldset p').textContent;
+
+const images = document.querySelectorAll('#thumb-list img');
+console.log(images);
+for (let img of images) {
+   img.style.boxShadow = "6px 5px 20px 1px rgba(0,0,0,0.22)";
+};
+
+// const d = document.querySelector('#msg');
+// // d.innerHTML = '<h2><HELLO WORLD</h2>';
+
+// // dispatchEvent.innerHTML = '<a href="#"><img src="ab.gif">'
+// d.insertAdjacentText("HELLO AGAIN");
